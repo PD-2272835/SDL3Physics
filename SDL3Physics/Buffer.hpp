c04aee2b99c5073b766b0b70c7c1d04325c84db0
PD-2272835 <186196474+PD-2272835@users.noreply.github.com>
2026-05-02 23:55:52 +0100

    very WIP Buffer header

--- SDL3Physics/Buffer.hpp
+++ SDL3Physics/Buffer.hpp
@@ -1,9 +1,22 @@
 #ifndef _BUFFER_HPP_
 #define _BUFFER_HPP_
+#include <SDL3/SDL_gpu.h>
 
 class Buffer
 {
-	Buffer();
+	SDL_GPUDevice* device;
+	SDL_GPUBufferCreateInfo Info;
+	SDL_GPUBuffer* ID;
+	bool isTransferring;
+
+	Buffer(SDL_GPUDevice device, Uint32 size, Uint8 usage);
+
+	//you must call EndTransfer() and must not deallocate the returned ptr
+	void* GetUploadHandle(Uint32 size);
+	void* GetDownloadHandle(Uint32 size);
+	
+	void EndUpload(int transferBufferOffset, int targetRegionOffset);
+	void EndDownload();
 };
 
 #endif
